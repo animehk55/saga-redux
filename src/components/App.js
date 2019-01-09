083a49e0d385b0320d06b96b392d9921d18501a8
Animesh Kumar <animeshk@webelight.com>
2019-01-09 18:47:46 +0530

    Added all the codes

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {getUsersRequest} from '../actions/users';
+import UsersList from './UsersList';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.props.getUsersRequest();
+  }
+
+  render() {
+    const users = this.props.users;
+    return (
+      <div style={{ margin: '0 auto', padding: '20px', maxWidth: '600px'}}>
+        <UsersList users={users.items} />
+      </div>
+    );
+  }
+}
+
+export default connect(null, {
+  getUsersRequest
+})(App);
